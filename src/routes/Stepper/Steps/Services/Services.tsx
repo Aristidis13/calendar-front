f723e92124717d46fc153676a425b759fda92bee
Aris Barlos <aris13pat@hotmail.com>
2025-03-13 00:15:50 +0200

    Adds photos and service data handling

--- src/routes/Stepper/Steps/Services/Services.tsx
+++ src/routes/Stepper/Steps/Services/Services.tsx
@@ -1,33 +1,41 @@
-import { Collapse, Typography } from 'antd';
+import { Collapse } from 'antd';
 import { services } from './ServicesMock';
-import Service from './Service';
-
-/**
- * Handles the services optional Step
- * @returns @Component
- */
-const Services = () => {
-  // Fetch the Mocks
-  return (
-    <div>
-      {services.services.map((service) => (
-        <Service service={service} />
-      ))}
-    </div>
-  );
-};
+import CLASSES from './styles.module.css';
+import ServiceCards from './ServiceCards';
+import { DownOutlined, ScissorOutlined } from '@ant-design/icons';
+import Title from './Title';
 
 /**
  * The Collapse that contains a title and Services
  */
-const ServicesCollapse = () => {
+const Services = () => {
   return (
     <Collapse
+      className={CLASSES.servicesContainer}
       defaultActiveKey={0}
+      bordered={false}
       expandIconPosition="end"
-      items={[{ key: 0, label: <Typography>{services.title}</Typography>, children: <Services /> }]}
+      expandIcon={({ isActive }) => (
+        <DownOutlined width={15} height={15} className={CLASSES.icon} rotate={isActive ? 0 : -90} />
+      )}
+      items={[
+        {
+          key: 0,
+          label: (
+            <Title
+              title={
+                <>
+                  <ScissorOutlined className={CLASSES.labelIcon} /> SERVICES
+                </>
+              }
+              secondaryTitle={services.title}
+            />
+          ),
+          children: <ServiceCards services={services.services} />,
+        },
+      ]}
     />
   );
 };
 
-export default ServicesCollapse;
+export default Services;
